2b48c7de64e905dce30c9fc3ca5567f9069a01c2
Leo_hsu <leo_hsu@gemteks.com>
2019-07-06 09:44:23 +0800

    Initial Commit

---
+++ testGPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGPS
+//
+//  Created by Leo_hsu on 2019/7/6.
+//  Copyright © 2019 Leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
